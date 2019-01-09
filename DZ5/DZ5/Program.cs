1ed26ef1a056e188bb38e176e45fbb77314110d8
Niki <super.nikita0308@gmail.com>
2019-01-09 12:57:13 +0300

    .

---
+++ DZ5/DZ5/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ5
+{   
+    class Student
+    {
+        private int year;
+
+        public int Year
+        {
+            get
+            {
+                return year;
+            }
+            set
+            {
+                if (value < 1)
+                    year = 1;
+                else if (value > 5)
+                    year = 5;
+                else year = value;
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student st1 = new Student();
+
+            st1.Year = 6;// записываем в поле, используя аксессор set
+            Console.WriteLine(st1.Year);
+        }
+    }
+}
